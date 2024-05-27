e67ae57d116066ff915f6f72480fb53d53fc496c
Rayngumi <rayngumi25@gmail.com>
2024-05-27 09:45:52 +0300

    complete assignment

--- lib/list_comprehension.py
+++ lib/list_comprehension.py
@@ -1,7 +1,11 @@
 #!/usr/bin/env python3
 
 def return_evens(num_list):
-    pass
+    if len(num_list) == 0:
+        return []
+    return [num for num in num_list if num % 2 == 0]
 
 def make_exclamation(sentence_list):
-    pass
+    if len(sentence_list) == 0:
+        return []
+    return [sentence + "!" for sentence in sentence_list]
